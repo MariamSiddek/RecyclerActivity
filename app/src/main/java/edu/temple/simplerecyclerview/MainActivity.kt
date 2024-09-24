2a4706f656f852568e5f99a4d510cf102a49e228
mariam44ES <137586760+mariam44ES@users.noreply.github.com>
2024-09-24 15:19:31 -0400

    Recycling task

--- app/src/main/java/edu/temple/simplerecyclerview/MainActivity.kt
+++ app/src/main/java/edu/temple/simplerecyclerview/MainActivity.kt
@@ -2,6 +2,7 @@ package edu.temple.simplerecyclerview
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
@@ -13,8 +14,16 @@ class MainActivity : AppCompatActivity() {
         val recyclerView = findViewById<RecyclerView>(R.id.recyclerView)
 
         //TODO Step 2: Provide a LayoutManager
-
+        val layoutManager = LinearLayoutManager(this)
+        recyclerView.layoutManager = layoutManager
 
         //TODO Step 4: Provide a RecyclerView.Adapter
+        val numbers = intArrayOf(12,20, 22, 24, 36, 48,50, 55, 60 , 70, 80)
+
+        // Create an instance of the adapter
+        val adapter = NumberDisplayAdapter(numbers)
+
+        // Attach the adapter to the RecyclerView
+        recyclerView.adapter = adapter
     }
 }
